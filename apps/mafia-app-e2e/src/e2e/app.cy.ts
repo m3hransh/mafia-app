4090f399adb2b4f04de93e039978de8b057c61c5
Mehran Shahidi <m.mehrashahidi@gmail.com>
2024-04-17 10:45:21 +0200

    🧪 Add e2e tests

--- apps/mafia-app-e2e/src/e2e/app.cy.ts
+++ apps/mafia-app-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,24 @@
-import { getGreeting } from '../support/app.po';
 
 describe('mafia-app-e2e', () => {
-  beforeEach(() => cy.visit('/'));
+  // beforeEach(()=> {});
 
-  it('should display welcome message', () => {
+  it('should go through game setup', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    cy.visit('/')
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    cy.get('h1').contains(/Mafia/);
+
+    // find input and enter a number
+    // input label should be 'Number of Players'
+    cy.get('label').contains('Number of Players');
+    cy.get('input').type('5'); 
+
+    // find a button with the text 'Start Game' and click it
+    cy.get('button').contains('Start Game').click();
+    // check if the url is '/game-setup'
+    cy.url().should('include', '/game-setup');
+
+    // check number of players is 5
+    cy.get('h2').contains(/Number of Players: 5/);
   });
 });
